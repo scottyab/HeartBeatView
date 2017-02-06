9074d3a0167930f0ac9e089a33d6dfbe748d6da2
Scott Alexander-Bown <dev@scottyab.com>
2017-02-06 16:52:09 +0000

    Added info dialog to sample app

--- app/src/main/java/com/scottyab/heartbeatview/sample/MainActivity.java
+++ app/src/main/java/com/scottyab/heartbeatview/sample/MainActivity.java
@@ -1,8 +1,14 @@
 package com.scottyab.heartbeatview.sample;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
@@ -17,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private HeartBeatView heartbeat3;
     private SeekBar beatsPerMinSeek;
     private TextView bpmTV;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,4 +71,45 @@ public class MainActivity extends AppCompatActivity {
         heartbeat3.toggle();
     }
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_info) {
+            showInfoDialog();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    private void showInfoDialog() {
+        AlertDialog infoDialog = new AlertDialog.Builder(this)
+                .setTitle(R.string.app_name)
+                .setMessage(R.string.info_details)
+                .setCancelable(true)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                })
+                .setNegativeButton("Github link", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        startActivity(new Intent(Intent.ACTION_VIEW,
+                                Uri.parse("https://github.com/scottyab/HeartBeatView")));
+                    }
+                })
+                .create();
+        infoDialog.show();
+    }
 }
